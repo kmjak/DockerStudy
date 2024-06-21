5cdf435bc7812b1f6c410b8aba963d5c3e8b4199
kmjak <tymkoron1221@icloud.com>
2024-06-22 05:50:23 +0900

    データベースの通信成功

--- php_ver2/login/php/index.php
+++ php_ver2/login/php/index.php
@@ -1,3 +1,9 @@
 <?php
-
-echo "hello world";
+$pdo = new PDO('mysql:host=db;dbname=users', 'root', 'admin');
+$sql = "select * from user_info";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$datas = $stmt->fetchAll();
+foreach ($datas as $data) {
+    echo "<p>名前 : {$data["name"]}, パスワード : {$data["pass"]}</p>";
+}
